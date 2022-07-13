f72a3f3d93038799d6c5c717b939e8b9550219a7
Your Name <webcgi@naver.com>
2022-07-13 12:19:42 +0900

    first commit

---
+++ marketplace-frontend-ph/src/constants/index.js
@@ -0,0 +1,28 @@
+export const TokenAddress = '0x4AD910956D7E08cC9b2BB0e991c9998ee86DDB8d';
+export const FactoryAddress = '0xa622BBa6700C063168feada0F5fba64BdEe0F876';
+export const AuctionAddress = '0x5702D005124b0EeF673202fd5d18ed7a012172e7';
+export const AuctionBNBAddress = '0x0A269272686D566Ca55D0BF88aBf0f3D4422f730';
+
+export const NFTStorageKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUxODlEMzhCRDVmMWE5MmU0RjIzNUVmMDExRTMzMjRiNUZDRkNjZTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDgwNDI1MjI0OSwibmFtZSI6Im1vb25zdGFyIn0.ioVYvZ_sYN-a89eGkMYJNPK-kRVx9vt0CdxGfQmtqg0';
+export const DefaultChainID = 97;
+export const ChainList = {
+  1: 'Mainnet',
+  3: 'Ropsten',
+  4: 'RinkeBy',
+  5: 'Goerli',
+  56: 'BSC mainnet',
+  97: 'BSC Testnet',
+};
+
+export const DefaultAvatar =
+  'https://gateway.pinata.cloud/ipfs/QmNjNR8dgjB8deVYR3HeUuZ4KvrsCut4uaxnRKaGysfS7K';
+
+export const CoinGeckoAPIForBNB =
+  'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd';
+
+export const CoinGeckoAPIForAcent =
+  'https://api.coingecko.com/api/v3/simple/price?ids=ACENT&vs_currencies=usd';
+//
+
+//0x4AD910956D7E08cC9b2BB0e991c9998ee86DDB8d
